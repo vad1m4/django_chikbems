68090fea546fffe60e0c0c7be913bbd6fd14527d
vad1m4 <vad.levitskiy@gmail.com>
2022-10-26 00:42:26 +0300

    minor changes

--- mysite/shop/urls.py
+++ mysite/shop/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from .views import home, product_detail
+from .views import home, product_detail, template
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path("", home, name="home"),
     path("products/<int:pk>", product_detail, name="product_detail"),
+    path("template/", template, name="template"),
+    path("contacts/", template, name="comtact-info"),
+    path("location/", template, name="location"),
+    path("products/", template, name="products"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -10,3 +10,15 @@ usd = 37
 def product_detail(request, pk):
     product = Product.objects.get(pk=pk)
     return render(request, "product.html", {"product": product, "usd": usd})
+
+def template(request):
+    return render(request, "template.html",)
+
+def contacts(request):
+    return render(request, "contact-info.html",)
+
+def location(request):
+    return render(request, "location.html",)
+
+def products(request):
+    return render(request, "products.html",)
