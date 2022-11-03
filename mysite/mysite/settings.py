f35be2dd6cc3f7bb81205a74c04ce2d34acb81d8
vad1m4 <vad.levitskiy@gmail.com>
2022-11-03 10:18:49 +0200

    products page 70% done
    more minor changes to other pages

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "shop",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Product
+from .filters import ProductFilter
 
 def home(request):
     products = Product.objects.all().order_by("-id")
@@ -21,4 +22,40 @@ def location(request):
     return render(request, "location.html",)
 
 def products(request):
-    return render(request, "products.html",)
+    products = Product.objects.all()
+
+    type_filter = request.GET.get("type","")
+    if type_filter == "universal":
+        products = products.filter(type="Universal")
+        print("UNIVERSAL")
+    elif type_filter == "outdoors":
+        products = products.filter(type="Outdoors")
+    elif type_filter == "indoors":
+        products = products.filter(type="Indoors")
+    # types = request.GET.getlist('type')
+    # for type in types:
+    #     products = products.filter(type=type)
+
+    has_nv_filter = request.GET.get("has_nv", "")
+    if has_nv_filter == 'yes':
+        products = products.filter(has_nightvision=True)
+    elif has_nv_filter == 'no':
+        products = products.filter(has_nightvision=False)
+
+    sort_by = request.GET.get("sort", "new-to-old")
+    if sort_by == "new-to-old":
+        products = products.order_by("-id")
+    elif sort_by == "old-to-new":
+        products = products.order_by("id")
+    elif sort_by == "high-to-low":
+        products = products.order_by("-price")
+    elif sort_by == "low-to-high":
+        products = products.order_by("price")
+
+    products_filter = ProductFilter(request.GET, queryset=products)
+    context = {
+        "products": products, 
+        "usd": usd,
+        "products_filter": products_filter,
+    }
+    return render(request, "products.html", context)
